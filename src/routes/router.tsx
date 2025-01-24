d01deab09fd27953e42a331a50e75eec5300cc73
Vladislav Bykanov <vbykanov1989@outlook.com>
2025-01-24 15:02:03 +0100

    wip

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,16 +1,18 @@
 import { Routes, Route, BrowserRouter } from 'react-router';
-import App from '../App';
 import Home from '../pages/home/Home';
-import LoginComponent from '@/pages/login/Login';
+import React from 'react';
+
+const LoginComponent = React.lazy(() => import('@/pages/login/Login'));
+const AppComponent = React.lazy(() => import('@/App'));
 
 export default function Router() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<App />}>
+				<Route path='/' element={<AppComponent />}>
 					<Route index element={<Home />} />
 				</Route>
-				<Route path='login' element={<LoginComponent/>} />
+				<Route path='login' element={<LoginComponent />} />
 				<Route path='*' element={<div>404 Not Found</div>} />
 			</Routes>
 		</BrowserRouter>
